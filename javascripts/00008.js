9f5800f23c9c19a82e1080e9b0ff61870e1a832d
andreschuelein <andreschuelein@gmail.com>
2016-01-31 18:12:43 +0100

    e8: added tooltip on hover

--- javascripts/00008.js
+++ javascripts/00008.js
@@ -1,32 +1,3 @@
-// var width = 1000,
-//     height = 800;
-// d3.select("#sContainer")
-//     .attr("width", width)
-//     .attr("height", height);
-
-// var svg = d3.select("#sContainer").append("svg")
-//     .attr("width", width)
-//     .attr("height", height);
-// var projection = d3.geo.albers()
-//     .center([0, 55.4])
-//     .rotate([4.4, 0])
-//     .parallels([50, 60])
-//     .scale(135000)
-//     .translate([-24750, -3215]);
-// var path = d3.geo.path()
-//     .projection(projection);
-
-// d3.json("../mapdata/00008/berlin_build.json", function(error, map) {
-//     if (error) {
-//         return console.error(error);
-//     } else {
-//         svg.append("path")
-//             .datum(topojson.feature(map, map.objects.berlin_transformed))
-//             .attr("d", path);
-
-//     }
-// });
-
 var width = 1000,
     height = 800;
 d3.select("#sContainer")
@@ -52,10 +23,24 @@ var projection = d3.geo.albers()
 var path = d3.geo.path()
     .projection(projection);
 
-var tooltip = d3.select("#sContainer")
-    .append('div')
-    .attr('class', 'tooltip')
-    .style('opacity', 1);
+tooltip = {
+    obj: d3.select("#sContainer")
+        .append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0)
+        .on('mouseover', (function(event) {
+            d3.event.preventDefault();
+        })),
+    updatePos: function(_x, _y) {
+        tooltip.obj
+            .style('left', (_x + 15) + 'px')
+            .style("top", (_y - 30) + "px");
+    },
+    updateLabel: function(_subdistrict, _district) {
+        tooltip.obj
+            .html(_subdistrict + ',</br>' + _district);
+    }
+};
 
 d3.json("../mapdata/00008/berlin_build_by_OTEIL.json", function(error, map) {
     if (error) {
@@ -74,7 +59,6 @@ d3.json("../mapdata/00008/berlin_build_by_OTEIL.json", function(error, map) {
         //define district boundaries    
         svg.append('path')
             .datum(topojson.mesh(map, map.objects.berlin_transformed, function(a, b) {
-                // console.log(a.properties.BEZIRK + " " + b.properties.BEZIRK);
                 return a.properties.BEZIRK !== b.properties.BEZIRK;
             }))
             .attr('d', path)
@@ -83,24 +67,28 @@ d3.json("../mapdata/00008/berlin_build_by_OTEIL.json", function(error, map) {
         //define subdistrict boundaries    
         svg.append('path')
             .datum(topojson.mesh(map, map.objects.berlin_transformed, function(a, b) {
-                // console.log(a.properties.BEZIRK + " " + b.properties.BEZIRK);
                 return a.properties.BEZIRK === b.properties.BEZIRK;
             }))
             .attr('d', path)
             .attr('class', 'subdistrict_boundary');
 
         // attach mouse events for tooltip
-        // svg.selectAll('.berlin')
-        // .on('mouseover', function(d) {
-        // console.log(d.id + ', ' + d3.event.pageX + ', ' + d3.event.pageY);
-        // tooltip.transition()
-        // .attr('opacity', 0.6);
-        // tooltip.moveToFront();
-        // tooltip
-        // .style("left", (d3.event.pageX) + "px")
-        // .style("top", (d3.event.pageY - 28) + "px")
-        // .html(d.id + ',</br>' + d.properties.BEZIRK)
-        // ;
-        // });
+        svg.selectAll('.berlin')
+            .on('mousemove', function(_e) {
+                var m = d3.mouse(this);
+                tooltip.updatePos(m[0], m[1]);
+            })
+            .on('mouseover', function(d) {
+                tooltip.obj
+                    .transition()
+                    .style('opacity', 1);
+
+                tooltip.updateLabel(d.id, d.properties.BEZIRK);
+            })
+            .on('mouseleave', function(d) {
+                tooltip.obj
+                    .transition()
+                    .style('opacity', 0);
+            });
     }
 });
